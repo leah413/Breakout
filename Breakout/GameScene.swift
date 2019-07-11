3d360d62d2453682327ccf9175c9e14c340b328e
Leah Nella <leahnella@yahoo.com>
2019-07-11 09:54:59 -0500

    Created bar object

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -12,10 +12,12 @@ import GameplayKit
 class GameScene: SKScene {
     
     var ball = SKShapeNode()
+    var paddle = SKSpriteNode()
     
     override func didMove(to view: SKView) {
         createBackground()
         makeBall()
+        makePaddle()
     }
     
     func createBackground() {
@@ -57,5 +59,13 @@ class GameScene: SKScene {
         ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.collisionBitMask)!
         
         addChild(ball) // add ball object to the view
+    }
+    func makePaddle() {
+        paddle = SKSpriteNode(color: UIColor.white, size: CGSize(width: frame.width/4, height: 20))
+        paddle.position = CGPoint(x: frame.midX, y: frame.minY + 125)
+        paddle.name = "paddle"
+        paddle.physicsBody = SKPhysicsBody(rectangleOf: paddle.size)
+        paddle.physicsBody?.isDynamic = false
+        addChild(paddle)
     }
 }
